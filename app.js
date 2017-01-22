ea33549f3b8aaf38f37b58e70bc4b14a5191f844
Jake Worthington <2003jakesw@gmail.com>
2017-01-22 11:45:12 +0000

    created the cannonball object

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 function loop() {
-    console.log ("hello world");
+    cannonBall.move();
+    console.log (cannonBall.x,cannonBall.y);
     setTimeout(loop, 1000)
 }
 
+var cannonBall = {
+    x:0,
+    y:0,
+    move: function (){
+        this.x++;
+        this.y++;
+    }
+} ;
 loop ();

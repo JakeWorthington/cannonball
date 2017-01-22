065e6d98eb99a91ee0c8a67f58a4ad57771e250b
Jake Worthington <2003jakesw@gmail.com>
2017-01-22 12:00:11 +0000

    added a drawing to the local host

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function loop() {
     cannonBall.move();
+    draw();
     console.log (cannonBall.x,cannonBall.y);
     setTimeout(loop, 1000)
-}
 
+}
 var cannonBall = {
     x:0,
     y:0,
@@ -12,4 +13,20 @@ var cannonBall = {
         this.y++;
     }
 } ;
-loop ();
+
+
+//drawing below
+
+var drawingSizeX = 400;
+var drawingSizeY = 400;
+var drawing = SVG('drawing').size(drawingSizeX, drawingSizeY);
+var rect = drawing.rect(drawingSizeX, drawingSizeY).attr({ fill: '#f06' });
+var circle = drawing.circle(10);
+
+function draw (){
+     circle.x(cannonBall.x);
+  circle.y(cannonBall.y);
+}
+
+//start running
+loop();

d1298fb213aebee47c24564d723aded96219bff4
Jake Worthington <2003jakesw@gmail.com>
2017-02-07 20:18:05 +0000

    changed the direction of the x axis so the cannon ball goes the other way.

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var cannonBall = {
     x:0,
     y:0,
     move: function (){
-        this.x++;
-        this.y++;
+        this.x = this.x + 10;
+        this.y = this.y + 10;
     }
 } ;
 
@@ -19,13 +19,15 @@ var cannonBall = {
 
 var drawingSizeX = 400;
 var drawingSizeY = 400;
+var realWidth = 1000;
+var ratio = realWidth / drawingSizeX;
 var drawing = SVG('drawing').size(drawingSizeX, drawingSizeY);
 var rect = drawing.rect(drawingSizeX, drawingSizeY).attr({ fill: '#f06' });
 var circle = drawing.circle(10);
 
 function draw (){
-     circle.x(cannonBall.x);
-  circle.y(cannonBall.y);
+     circle.x(cannonBall.x * ratio);
+  circle.y(drawingSizeY - cannonBall.y * ratio);
 }
 
 //start running

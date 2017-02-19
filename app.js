1dee25b579cde0a1f38e35a4424b0e8e61c58f87
Jake Worthington <2003jakesw@gmail.com>
2017-02-19 16:16:16 +0000

    we made that were ever the circle goes it draws a line and the line is in the middle of the circle, we made it so the frist line from 0,0 doesn't draw

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ var interval = 100;
 function loop() {
     cannonBall.move(interval / 1000);
     draw();
-    console.log (cannonBall.x,cannonBall.y);
+    console.log(cannonBall.x, cannonBall.y);
     setTimeout(loop, interval)
 
 }
 var cannonBall = {
-    speed : 10, 
-   direction : 0.1 * Math.PI,
-   x:0,
-    y:100,
-    move: function (time){
-        var distanceTravelled = this.speed * time; 
+    speed: 10,
+    direction: 0.1 * Math.PI,
+    x: 0,
+    y: 100,
+    move: function (time) {
+        var distanceTravelled = this.speed * time;
         var deltaX = Math.cos(this.direction) * distanceTravelled;
         var deltaY = Math.sin(this.direction) * distanceTravelled;
-        this.x = this.x + deltaX; 
+        this.x = this.x + deltaX;
         this.y = this.y + deltaY;
     }
-} ;
+};
 
 
 //drawing below
@@ -31,12 +31,17 @@ var ratio = drawingSizeX / realWidth;
 var drawing = SVG('drawing').size(drawingSizeX, drawingSizeY);
 var rect = drawing.rect(drawingSizeX, drawingSizeY).attr({ fill: '#f06' });
 var circle = drawing.circle(10);
+var firstDraw = true;
 
-function draw (){
+function draw() {
     var newX = cannonBall.x * ratio;
     var newY = drawingSizeY - cannonBall.y * ratio;
-     circle.x(newX);
-  circle.y(newY);
+    if (firstDraw != true) {
+        var line = drawing.line(circle.cx(), circle.cy(), newX, newY).stroke({ width: 1 });
+    }
+    circle.cx(newX);
+    circle.cy(newY);
+    firstDraw = false;
 }
 
 //start running
